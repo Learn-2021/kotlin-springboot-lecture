ada98b58f838188b3e359f29b338aaeed857410b
cocodori <moradulee@gmail.com>
2021-05-16 14:36:42 +0900

    Custom Validation

--- src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
+++ src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
@@ -1,5 +1,6 @@
 package com.example.mvc.model.http
 
+import com.example.mvc.controller.StringFormatDateTime
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
@@ -28,15 +29,17 @@ data class UserRequest(
     @field:Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}\$")  // 정규식 검증
     var phoneNumber: String? = null,
 
+    @field:StringFormatDateTime(pattern = "yyyy-MM-dd HH:mm:ss")
     var createAt: String? = null // yyyy-MM-dd HH:mm:ss Ex.2021-01-01 00:00:00
 ) {
-    @AssertTrue(message = "생성일자 패턴은 yyyy-MM-dd HH:mm:ss 이어야 합니다.")
-    private fun isValidCreateAt(): Boolean {
-        return try {
-            LocalDateTime.parse(this.createAt, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
-            true
-        } catch (e: Exception) {
-            false
-        }
-    }
+//    Custom Validator로 변경
+//    @AssertTrue(message = "생성일자 패턴은 yyyy-MM-dd HH:mm:ss 이어야 합니다.")
+//    private fun isValidCreateAt(): Boolean {
+//        return try {
+//            LocalDateTime.parse(this.createAt, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+//            true
+//        } catch (e: Exception) {
+//            false
+//        }
+//    }
 }
