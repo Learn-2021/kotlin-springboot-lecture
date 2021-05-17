ab3b440c74049e0b605f8b3747247c92234f0e2d
cocodori <moradulee@gmail.com>
2021-05-17 20:31:59 +0900

    ExceptionHandler를 활용하여 자세한 에러 반환하기

--- src/main/kotlin/com/example/mvc/advice/GlobalControllerAdvice.kt
+++ src/main/kotlin/com/example/mvc/advice/GlobalControllerAdvice.kt
@@ -1,14 +1,19 @@
 package com.example.mvc.advice
 
 import com.example.mvc.controller.exception.ExceptionApiController
+import com.example.mvc.model.http.Error
+import com.example.mvc.model.http.ErrorResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import java.lang.IndexOutOfBoundsException
 import java.lang.RuntimeException
+import java.time.LocalDateTime
+import javax.servlet.http.HttpServletRequest
+import javax.validation.ConstraintViolationException
 
-@RestControllerAdvice(basePackageClasses = [ExceptionApiController::class])
+//@RestControllerAdvice(basePackageClasses = [ExceptionApiController::class])
 class GlobalControllerAdvice {
 
     @ExceptionHandler(value = [RuntimeException::class])
